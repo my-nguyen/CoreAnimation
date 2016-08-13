7cd49b394519eaa5d6689c0f28c39fd854e0fb9f
My Nguyen <My@mys-macbook-pro.local>
2016-08-13 16:19:17 -0700

    Switch, case, animate: animateWithDuration

--- CoreAnimation/ViewController.swift
+++ CoreAnimation/ViewController.swift
@@ -29,6 +29,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tapped(sender: AnyObject) {
+        // hide the tap button so the animations won't collide
+        tap.hidden = true
+
+        // animateWithDuration() accepts 5 parameters
+        // (1) duration: NSTimeInterval - how long to animate for
+        // (2) delay: NSTimeInterval - how long to pause before the animation starts
+        // (3) options: UIViewAnimationOptions
+        // (4) animations: () -> Void - what animations to execute
+        // (5) completion: ((Bool) -> Void)?) - what to execute when the animation completes
+        let animations = { [unowned self] in
+            switch self.currentAnimation {
+            case 0:
+                break
+            default:
+                break
+            }
+        }
+        let completion = { [unowned self] (finished: Bool) in
+            // unhide the tap button
+            self.tap.hidden = false
+        }
+        // duration of 1 second, no delay, no options
+        UIView.animateWithDuration(1, delay: 0, options: [], animations: animations, completion: completion)
+
         // cycle through animations each time the button is tapped
         currentAnimation += 1
         if currentAnimation > 7 {
