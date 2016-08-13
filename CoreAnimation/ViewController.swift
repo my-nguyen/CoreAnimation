c6d9e9f3d08b63d50cbe8200ee3fbe08a2214a61
My Nguyen <My@mys-macbook-pro.local>
2016-08-13 16:07:22 -0700

    Preparing for action

--- CoreAnimation/ViewController.swift
+++ CoreAnimation/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tap: UIButton!
+    var imageView: UIImageView!
+    var currentAnimation = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // place a penguin in the middle of an iPad-sized landscape screen
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapped(sender: AnyObject) {
+        // cycle through animations each time the button is tapped
+        currentAnimation += 1
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
 }
-
